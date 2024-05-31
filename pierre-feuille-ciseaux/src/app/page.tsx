e860f240b33aa66351e3f884fb907f1b18e2b7b1
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-05-31 16:12:25 +0200

    modify the tap container and redesigned

--- pierre-feuille-ciseaux/src/app/page.tsx
+++ pierre-feuille-ciseaux/src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="bg-radial-gradient">
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="flex relative w-full h-80 bg-triangle">
-          <div className="flex flex-col absolute bottom-20 left-36 items-center">
+        <div className="flex relative w-72 h-80 bg-triangle">
+          <div className="flex flex-col absolute bottom-8 left-28  items-center">
             <Image
               src="/icon-rock.svg"
               alt="Pierre"
@@ -15,8 +15,8 @@ export default function Home() {
               className="cursor-pointer z-10"
             />
           </div>
-          <div className="w-24 h-24 bg-white absolute bottom-14 left-28 rounded-full border-8 border-red border-red-600"></div>
-          <div className="flex flex-col absolute top-14 left-14 items-center">
+          <div className="w-24 h-24 bg-white absolute bottom-0 left-20  rounded-full border-8 border-red border-red-600"></div>
+          <div className="flex flex-col absolute top-6 left-0 items-center">
             <Image
               src="/icon-paper.svg"
               alt="Feuille"
@@ -25,8 +25,8 @@ export default function Home() {
               className="cursor-pointer z-10"
             />
           </div>
-          <div className="w-24 h-24 bg-white absolute top-8 left-7 rounded-full border-8 border-blue border-blue-600"></div>
-          <div className="flex flex-col absolute top-14 right-14 items-center">
+          <div className="w-24 h-24 bg-white absolute top-0 mt-[-0.5rem] left-0 ml-[-2rem] rounded-full border-8 border-blue border-blue-600"></div>
+          <div className="flex flex-col absolute top-4 right-0 items-center">
             <Image
               src="/icon-scissors.svg"
               alt="Ciseaux"
@@ -35,7 +35,7 @@ export default function Home() {
               className="cursor-pointer z-10"
             />
           </div>
-          <div className="w-24 h-24 bg-white absolute top-8 right-7 rounded-full border-yellow border-shadow border-8 border-yellow-500"></div>
+          <div className="w-24 h-24 bg-white absolute top-0 mt-[-0.5rem] right-0 mr-[-2rem] rounded-full border-yellow border-shadow border-8 border-yellow-500"></div>
         </div>
       </main>
     </div>
